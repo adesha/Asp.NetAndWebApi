b2b326991ffdda176d4397c9f1d6ba624a590007
Adesha H P <Adesha.Hp@Solera.com>
2022-11-04 10:14:05 +0530

    Section 2

---
+++ AspWebApi/AspWebApi.net/Data/WebApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AspWebApi.net.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspWebApi.net.Data
+{
+    public class WebApiDbContext:DbContext 
+    {
+        public WebApiDbContext(DbContextOptions<WebApiDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
