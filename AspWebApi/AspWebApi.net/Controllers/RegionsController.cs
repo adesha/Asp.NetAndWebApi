2bda39481cfc023d037b71623b3834841f7e811a
Adesha H P <Adesha.Hp@Solera.com>
2022-11-04 14:20:24 +0530

    Section 4

--- AspWebApi/AspWebApi.net/Controllers/RegionsController.cs
+++ AspWebApi/AspWebApi.net/Controllers/RegionsController.cs
@@ -2,6 +2,7 @@
 using AspWebApi.net.Models.Domain;
 using AspWebApi.net.Repositories;
 using AutoMapper;
+using AspWebApi.net.Models.DTO;
 
 namespace AspWebApi.net.Controllers
 {
@@ -44,6 +45,119 @@ namespace AspWebApi.net.Controllers
             var regionsDTO=mapper.Map<List<Models.DTO.Region>>(regions);
 
             return Ok(regionsDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetRegionAsync")]
+        public async Task<IActionResult> GetRegionAsync(Guid id)
+        {
+            var region = await regionRepository.GetAsync(id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            var regionDTO=mapper.Map<Models.DTO.Region>(region);
+
+            return Ok(regionDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRegionAsync(Models.DTO.AddRegionRequest addRegionRequest)
+        {
+            //Request to domain model
+            var region = new Models.Domain.Region()
+            {
+                Code = addRegionRequest.Code,
+                Name = addRegionRequest.Name,
+                Area = addRegionRequest.Area,
+                Lat = addRegionRequest.Lat,
+                Long = addRegionRequest.Long,
+                Population = addRegionRequest.Population,
+            };
+
+            //pass details to repo
+            var response=await regionRepository.AddAsync(region);
+
+            //convert back to dto
+            var regionDTO = new Models.DTO.Region
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population,
+            };
+
+            return CreatedAtAction(nameof(GetRegionAsync), new { id = regionDTO.Id }, regionDTO);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteRegionAsync(Guid id)
+        {
+            //get region from database
+            var region=await regionRepository.DeleteAsync(id);
+            //if null notfound
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            //convert response back to dto
+            var regionDTO = new Models.DTO.Region
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population,
+            };
+            //return of response
+            return Ok(regionDTO);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateRegionAsync([FromRoute]Guid id,[FromBody]UpdateRegionRequest updateRegionRequest)
+        {
+            //convert dto domain model
+            var region = new Models.Domain.Region()
+            {
+                Code = updateRegionRequest.Code,
+                Name = updateRegionRequest.Name,
+                Lat = updateRegionRequest.Lat,
+                Long = updateRegionRequest.Long,
+                Area = updateRegionRequest.Area,
+                Population = updateRegionRequest.Population,
+            };
+            //update region using repository
+            var uregion = await regionRepository.UpdateAsync(id, region);
+            //if null then notfound
+            if(uregion == null)
+            {
+                return NotFound();
+            }
+            //convert domain back to dto
+            var regionDTO = new Models.DTO.Region()
+            {
+                Id = uregion.Id,
+                Name = uregion.Name,
+                Code = uregion.Code,
+                Long = uregion.Long,
+                Lat = uregion.Lat,
+                Population = uregion.Population,
+                Area = uregion.Area,
+            };
+
+            //return ok response
+            return Ok(regionDTO);
         }
     }
 }
--- AspWebApi/AspWebApi.net/Repositories/RegionRepository.cs
+++ AspWebApi/AspWebApi.net/Repositories/RegionRepository.cs
@@ -11,9 +11,56 @@ namespace AspWebApi.net.Repositories
         {
             this.webApiDbContext=webApiDbContext;
         }
+
         public async Task<IEnumerable<Region>> GetAllAsync()
         {
             return await webApiDbContext.Regions.ToListAsync();
+        }
+
+        public async Task<Region> GetAsync(Guid id)
+        {
+            return await webApiDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Region> AddAsync(Region region)
+        {
+            region.Id=Guid.NewGuid();
+            await webApiDbContext.AddAsync(region);
+            await webApiDbContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> DeleteAsync(Guid id)
+        {
+            var region = await webApiDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if (region == null)
+            {
+                return null;
+            }
+            //delete the region
+            webApiDbContext.Regions.Remove(region);
+            await webApiDbContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> UpdateAsync(Guid id, Region region)
+        {
+            var existingregion=await webApiDbContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(region == null)
+            {
+                return null;
+            }
+
+            existingregion.Code = region.Code;
+            existingregion.Name = region.Name;
+            existingregion.Area = region.Area;
+            existingregion.Lat = region.Lat;
+            existingregion.Long = region.Long;
+            existingregion.Population=region.Population;
+
+            await webApiDbContext.SaveChangesAsync();
+            return existingregion;
         }
     }
 }
