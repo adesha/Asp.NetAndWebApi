7438875e5367d67202ae4b053c43eb4c8246aabb
Adesha H P <Adesha.Hp@Solera.com>
2022-11-10 11:55:40 +0530

    Removes All Errors

--- AspWebApi/AspWebApi.net/Controllers/WalksController.cs
+++ AspWebApi/AspWebApi.net/Controllers/WalksController.cs
@@ -127,26 +127,26 @@ namespace AspWebApi.net.Controllers
         #region Private methods
         private async Task<bool> ValidateAddWalkAsync(Models.DTO.AddWalkRequest addWalkRequest)
         {
-            if (addWalkRequest == null)
-            {
-                ModelState.AddModelError(nameof(addWalkRequest), $"{nameof(addWalkRequest)}, cannot be empty.");
-                return false;
-            }
-            if (string.IsNullOrWhiteSpace(addWalkRequest.Name))
-            {
-                ModelState.AddModelError(nameof(addWalkRequest.Name), $"{nameof(addWalkRequest.Name)}, cannot be null or whitespace.");
-            }
-            if (addWalkRequest.Length <= 0)
-            {
-                ModelState.AddModelError(nameof(addWalkRequest.Length), $"{nameof(addWalkRequest.Length)}, cannot be less than or equal to zero.");
-            }
-            var region = regionRepository.GetAsync(addWalkRequest.RegionId);
+            //if (addWalkRequest == null)
+            //{
+            //    ModelState.AddModelError(nameof(addWalkRequest), $"{nameof(addWalkRequest)}, cannot be empty.");
+            //    return false;
+            //}
+            //if (string.IsNullOrWhiteSpace(addWalkRequest.Name))
+            //{
+            //    ModelState.AddModelError(nameof(addWalkRequest.Name), $"{nameof(addWalkRequest.Name)}, cannot be null or whitespace.");
+            //}
+            //if (addWalkRequest.Length <= 0)
+            //{
+            //    ModelState.AddModelError(nameof(addWalkRequest.Length), $"{nameof(addWalkRequest.Length)}, cannot be less than or equal to zero.");
+            //}
+            var region = await regionRepository.GetAsync(addWalkRequest.RegionId);
             if(region == null)
             {
                 ModelState.AddModelError(nameof(addWalkRequest.RegionId), $"{nameof(addWalkRequest.RegionId)}, is Invalid.");
             }
 
-            var walkd = walkDifficultyRepository.GetWalkDifficultyByIdAsync(addWalkRequest.WalkDifficultyId);
+            var walkd = await walkDifficultyRepository.GetWalkDifficultyByIdAsync(addWalkRequest.WalkDifficultyId);
             if (walkd == null)
             {
                 ModelState.AddModelError(nameof(addWalkRequest.WalkDifficultyId), $"{nameof(addWalkRequest.WalkDifficultyId)}, is Invalid.");
@@ -174,13 +174,13 @@ namespace AspWebApi.net.Controllers
             //{
             //    ModelState.AddModelError(nameof(updateWalkRequest.Length), $"{nameof(updateWalkRequest.Length)}, cannot be less than or equal to zero.");
             //}
-            var region = regionRepository.GetAsync(updateWalkRequest.RegionId);
+            var region = await regionRepository.GetAsync(updateWalkRequest.RegionId);
             if (region == null)
             {
                 ModelState.AddModelError(nameof(updateWalkRequest.RegionId), $"{nameof(updateWalkRequest.RegionId)}, is Invalid.");
             }
 
-            var walkd = walkDifficultyRepository.GetWalkDifficultyByIdAsync(updateWalkRequest.WalkDifficultyId);
+            var walkd = await walkDifficultyRepository.GetWalkDifficultyByIdAsync(updateWalkRequest.WalkDifficultyId);
             if (walkd == null)
             {
                 ModelState.AddModelError(nameof(updateWalkRequest.WalkDifficultyId), $"{nameof(updateWalkRequest.WalkDifficultyId)}, is Invalid.");
